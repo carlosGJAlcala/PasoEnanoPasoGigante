e117b07f2295b92e50cc5ae056aa225cb97ee940
carlos <carlos.garridoj@edu.uah.es>
2024-12-29 13:16:25 +0100

    funciona para todos menos para el ultimo numero

--- src/AlgoritmoPasoDeGigantePasoDeEnano.java
+++ src/AlgoritmoPasoDeGigantePasoDeEnano.java
@@ -21,9 +21,8 @@ public class AlgoritmoPasoDeGigantePasoDeEnano {
         BigInteger y = lectura.nextBigInteger();
         System.out.println("Empieza a ejecutar el programa y contabilizar el tiempo");
         long startTime = System.currentTimeMillis();
-       Long t = Math.round(Math.sqrt(m.doubleValue()));
-        Map<BigInteger, Integer> parejas = pasoGigante(g, m,t);
-        BigInteger resultado = pasoEnano(parejas, g, y, m,t);
+        Map<BigInteger, Integer> parejas = pasoGigante(g, m);
+        BigInteger resultado = pasoEnano(parejas, g, y, m);
         System.out.printf("El resultado de b es %s%n", resultado);
         long endTime = System.currentTimeMillis() -startTime;
         System.out.println("Duración " + endTime + " milisegundos.");
@@ -54,15 +53,10 @@ public class AlgoritmoPasoDeGigantePasoDeEnano {
                 new BigInteger("24056225665222"),
                 new BigInteger("12761818229206")
         };
-        Long t;
         long startTime = System.currentTimeMillis();
         for(int i=0;i<6;i++){
-             t = Math.round(Math.sqrt(mGrupo[i].doubleValue()));
-            if(i==5){
-                t= raiz(mGrupo[i]).longValue();
-            }
-            Map<BigInteger, Integer> parejas = pasoGigante(gGrupo[i], mGrupo[i],t);
-            BigInteger resultado = pasoEnano(parejas, gGrupo[i], yValores[i], mGrupo[i],t);
+            Map<BigInteger, Integer> parejas = pasoGigante(gGrupo[i], mGrupo[i]);
+            BigInteger resultado = pasoEnano(parejas, gGrupo[i], yValores[i], mGrupo[i]);
             System.out.printf("El resultado de b es %s%n para el valor de la pos: %d %n ", resultado,i);
             long endTime = System.currentTimeMillis() -startTime;
             System.out.println("Duración " + endTime + " milisegundos.");
@@ -82,39 +76,32 @@ public class AlgoritmoPasoDeGigantePasoDeEnano {
         modoTest();
     }
 
-    public static Map<BigInteger, Integer> pasoGigante(BigInteger g, BigInteger m,Long t) {
+    public static Map<BigInteger, Integer> pasoGigante(BigInteger g, BigInteger m) {
         Map<BigInteger, Integer> parejas = new HashMap<>();
-        for (int j = 0; j < t ;j++) {
+        double t = Math.ceil(Math.sqrt(m.doubleValue())); // sqrt(m)
+        for (int j = 0; j < t; j++) {
 
+            BigInteger temporalT = BigDecimal.valueOf(t).toBigInteger();
             BigInteger temporalJ = BigInteger.valueOf(j);
-            BigInteger resultadoTemp= temporalJ.multiply(BigInteger.valueOf(t)).mod(m.subtract(new BigInteger("1")));
+            BigInteger resultadoTemp= temporalJ.multiply(temporalT).mod(m.subtract(new BigInteger("1")));
             BigInteger valorT= g.modPow(resultadoTemp,m);
             parejas.put(valorT, j);
         }
         return parejas;
     }
 
-    public static BigInteger pasoEnano(Map<BigInteger, Integer>  parejas, BigInteger g, BigInteger y, BigInteger m,Long t) {
+    public static BigInteger pasoEnano(Map<BigInteger, Integer>  parejas, BigInteger g, BigInteger y, BigInteger m) {
+        double t = Math.ceil(Math.sqrt(m.doubleValue())); // sqrt(m)
         for (int i = 0; i < t; i++) {
             BigInteger valorTemporal=g.modPow(BigInteger.valueOf(i),m);
             BigInteger valor = valorTemporal.multiply(y).mod(m);
             if (parejas.containsKey(valor)) {
                 Integer j = parejas.get(valor);
-                return BigInteger.valueOf(j).multiply(BigInteger.valueOf(t).subtract(BigInteger.valueOf(i)).mod(m));            }
+                return BigInteger.valueOf(j).multiply(BigDecimal.valueOf(t).toBigInteger()).subtract(BigInteger.valueOf(i)).mod(m);            }
         }
         System.out.println("devuelve nulo");
         return null;
     }
 
-    public static BigInteger raiz(BigInteger x) {
-        BigInteger a= BigInteger.ZERO.setBit(x.bitLength()/2);
-        BigInteger b= a;
-        while(true) {
-            BigInteger c = a.add(x.divide(a)).shiftRight(1);
-            if (c.equals(a) || c.equals(b))
-                return c;
-            b= a;
-            a= c;
-        }
-    }
+
 }
